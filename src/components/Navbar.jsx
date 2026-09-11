e705e84f80aff5c714646e67a25690ef6b661844
nova-1029 <ankur.arora0622@gmail.com>
2026-09-11 11:39:52 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,25 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function Navbar() {
-  const cartItems = useSelector(
-    state => state.cart.items
-  );
+  const cartItems = useSelector((state) => state.cart.items);
 
-  const totalItems = cartItems.reduce(
+  const cartCount = cartItems.reduce(
     (total, item) => total + item.quantity,
     0
   );
 
   return (
     <nav className="navbar">
-      <Link to="/" className="logo">
-        🌱 Paradise Nursery
-      </Link>
+      <div className="navbar-brand">
+        <Link to="/">Paradise Nursery</Link>
+      </div>
 
-      <div className="nav-links">
+      <div className="navbar-links">
         <Link to="/">Home</Link>
         <Link to="/plants">Plants</Link>
         <Link to="/about">About Us</Link>
-
-        <Link to="/cart" className="cart-link">
-          🛒 Cart
-          <span className="cart-count">
-            {totalItems}
-          </span>
+        <Link to="/cart">
+          🛒 Cart ({cartCount})
         </Link>
       </div>
     </nav>
