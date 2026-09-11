7800dd3ab259cbf64f9343a46f9a404270f23706
nova-1029 <ankur.arora0622@gmail.com>
2026-09-11 11:40:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import CartItem from "./pages/CartItem";
 import AboutUs from "./pages/AboutUs";
 
 import "./App.css";
+
+function LandingPage() {
+  return (
+    <div className="background-image">
+      <div className="landing-content">
+        <h1>Welcome to Paradise Nursery</h1>
+
+        <p>
+          Discover beautiful plants and bring nature into your home.
+        </p>
+
+        <Link to="/plants" className="get-started">
+          Get Started
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -14,7 +31,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/plants" element={<ProductList />} />
         <Route path="/cart" element={<CartItem />} />
         <Route path="/about" element={<AboutUs />} />
